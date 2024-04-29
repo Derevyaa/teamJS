a05e569085989982f45a3b9ed1b49258df0429d6
derevyaa <aderevyanchenko@gmail.com>
2024-04-29 18:32:48 +0300

    code review files

---
+++ wrongCrudAPI.js
@@ -0,0 +1,33 @@
+function ManipulatedataInAPI(endpoint, params, data, action, authorization) {
+    let requestOptions = {
+        headers: {
+            'Authorization': authorization
+        }
+    };
+
+    if (action === 'GET') {
+        return fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .catch(error => console.error('Помилка отримання даних з API:', error));
+    } else if (action === 'DELETE') {
+        // Логіка видалення даних з API за допомогою переданих параметрів
+        return fetch(endpoint, { ...requestOptions, method: 'DELETE' })
+            .catch(error => console.error('Помилка видалення даних з API:', error));
+    } else if (action === 'POST') {
+        // Логіка створення даних в API за допомогою переданих параметрів
+        requestOptions.method = 'POST';
+        requestOptions.headers['Content-Type'] = 'application/json';
+        requestOptions.body = JSON.stringify(data);
+        return fetch(endpoint, requestOptions)
+            .catch(error => console.error('Помилка створення даних в API:', error));
+    } else if (action === 'PUT') {
+        // Логіка модифікації даних в API за допомогою переданих параметрів
+        requestOptions.method = 'PUT';
+        requestOptions.headers['Content-Type'] = 'application/json';
+        requestOptions.body = JSON.stringify(data)
+        return fetch(endpoint, requestOptions)
+            .catch(error => console.error('Помилка модифікації даних в API:', error));
+    } else {
+        console.error('Невідома операція');
+    }
+}
